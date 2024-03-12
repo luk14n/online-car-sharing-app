b72be0308250f8a2464133af5c20820e19de9ca7
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-03-12 10:32:27 +0200

    init commit

---
+++ src/main/java/com/lukian/onlinecarsharing/OnlineCarSharingApplication.java
@@ -0,0 +1,13 @@
+package com.lukian.onlinecarsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCarSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCarSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukian/onlinecarsharing/OnlineCarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukian.onlinecarsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCarSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
